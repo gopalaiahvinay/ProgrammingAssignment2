bc794abe9386be5718f40858f2c6f0bde2cb5713
gopalaiahvinay <gopalaiahvinay@gmail.com>
2014-11-23 14:52:12 -0600

    Try 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,41 +1,62 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+#Global Matrix
+values = c(1,5,4,3,2,6,7,5,0)
+A = matrix(values,3,3)
+# Minor and cofactor to calculate Adjoint Matrix
+minor <- function(A, i, j) det( A[-i,-j] )
+cofactor <- function(A, i, j) (-1)^(i+j) * minor(A,i,j)
+
+cacheA = NULL;
 ## Write a short comment describing this function
 
+
+#Just returns the cache value 
 makeCacheMatrix <- function(x = matrix()) {
   
-  makeVector <- function(x = numeric()) {
-    m <- NULL
-    set <- function(y) {
-      x <<- y
-      m <<- NULL
-    }
-    get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
-    list(set = set, get = get,
-         setmean = setmean,
-         getmean = getmean)
-  }
+      if(!is.null(cacheA)){
+        print("Fetching from cache")
+        return(cacheA)
+      }
+      else{
+        
+        cacheA = cacheSolve(A);
+        
+      }
+  
 
 }
+
+#function to calculate only adjoincy
+adjoint1 <- function(A) {
+  n <- nrow(A)
+  B <- matrix(NA, n, n)
+  for( i in 1:n )
+    for( j in 1:n )
+      B[j,i] <- cofactor(A, i, j)
+  B
+}
+
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+#Computes the inversion
+cacheSolve <- function(A) {
         ## Return a matrix that is the inverse of 'x'
+    
+  #Adjoint of A 
+  adjA = adjoint1(A);
+  #Inverse determinant of A
+  detInvA = 1/det(A)
   
-  cachemean <- function(x, ...) {
-    m <- x$getmean()
-    if(!is.null(m)) {
-      message("getting cached data")
-      return(m)
-    }
-    data <- x$get()
-    m <- mean(data, ...)
-    x$setmean(m)
-    m
-  }
+  #Inverse Matrix 
+  invA = detInvA * adjA
+  return(invA)
+  
 }
+
+result = makeCacheMatrix(A)
+
+
+
